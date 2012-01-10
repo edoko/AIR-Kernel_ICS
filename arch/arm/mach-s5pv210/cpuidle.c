4e9915dc1c8b42c406607f1e40b1a6aa70ec15ce
Steve Garon <steve.garon@gmail.com>
2012-01-09 22:13:30 -0500

    Fix S5pv210 cpuidle driver for 3.2 port 2/2

--- arch/arm/mach-s5pv210/cpuidle.c
+++ arch/arm/mach-s5pv210/cpuidle.c
@@ -130,7 +130,7 @@ static int loop_sdmmc_check(void)
 =======
 /* Actual code that puts the SoC in different idle states */
 static int s5p_enter_idle_normal(struct cpuidle_device *dev,
-				struct cpuidle_state *state,
+			struct cpuidle_driver *drv,
 			      int index)
 >>>>>>> 331dda4... Fix S5pv210 cpuidle driver for 3.2 port
 {
