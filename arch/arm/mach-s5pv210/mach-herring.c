17ff3feb81b0186341daf4afe00300de8ff1f7c9
edoko <r_data@naver.com>
2012-02-01 19:17:00 +0900

    Fixed mach-herring.c

--- arch/arm/mach-s5pv210/mach-herring.c
+++ arch/arm/mach-s5pv210/mach-herring.c
@@ -459,23 +459,8 @@ static struct s5p_media_device herring_media_devs[] = {
 #ifdef CONFIG_CPU_FREQ
 static struct s5pv210_cpufreq_voltage smdkc110_cpufreq_volt[] = {
 	{
-		.freq	= 1400000,
-		.varm	= 1450000,
-		.vint	= 1200000,
-	},
-	{
-		.freq	= 1200000,
-		.varm	= 1325000,
-		.vint	= 1150000,
-	},
-	{
-		.freq	= 1100000,
-		.varm	= 1275000,
-		.vint	= 1100000,
-	},
-	{
 		.freq	= 1000000,
-		.varm	= 1250000,
+		.varm	= 1275000,
 		.vint	= 1100000,
 	}, {
 		.freq	=  800000,
@@ -491,7 +476,7 @@ static struct s5pv210_cpufreq_voltage smdkc110_cpufreq_volt[] = {
 		.vint	= 1100000,
 	}, {
 		.freq	=  100000,
-		.varm	=  925000,
+		.varm	=  950000,
 		.vint	= 1000000,
 	},
 };
