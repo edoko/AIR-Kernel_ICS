1c30877012b529a28b7fd04dfdd6710f51f1ae39
edoko <r_data@naver.com>
2012-03-14 17:49:43 +0900

    Added OndemandX governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -397,6 +397,9 @@ extern struct cpufreq_governor cpufreq_gov_wheatley;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SAVAGEDZEN)
 extern struct cpufreq_governor cpufreq_gov_savagedzen;
 #define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_savagedzen)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ONDEMANDX)
+extern struct cpufreq_governor cpufreq_gov_ondemandx;
+#define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_ondemandx)
 #endif
 
 
