feb931273ec15737b9b57ddbf2c5f9fcbcb88dc1
edoko <r_data@naver.com>
2012-04-13 22:56:02 +0900

    Increased min readahead 16 to 32

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -1470,7 +1470,7 @@ void task_dirty_inc(struct task_struct *tsk);
 
 /* readahead.c */
 #define VM_MAX_READAHEAD	1024	/* kbytes */
-#define VM_MIN_READAHEAD	16	/* kbytes (includes current page) */
+#define VM_MIN_READAHEAD	32	/* kbytes (includes current page) */
 
 extern unsigned long max_readahead_pages;
 
