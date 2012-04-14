3077b8b4622a7a8d6b8879370bda7a3aaea7b5c7
edoko <r_data@naver.com>
2012-04-14 19:12:21 +0900

    Revert "Revert "Sleep freezing fix (Thanks to Michael Huang, netarchy)""
    This reverts commit 2d3c85673bac74fe7b66457ed4aab26c4be6202c.

--- arch/arm/mach-s5pv210/cpufreq.c
+++ arch/arm/mach-s5pv210/cpufreq.c
@@ -837,15 +837,24 @@ static int __init s5pv210_cpu_init(struct cpufreq_policy *policy)
 static int s5pv210_cpufreq_notifier_event(struct notifier_block *this,
 		unsigned long event, void *ptr)
 {
+	static int max, min;
 	int ret;
+
+	struct cpufreq_policy *policy = cpufreq_cpu_get(0);
 
 	switch (event) {
 	case PM_SUSPEND_PREPARE:
 #ifdef CONFIG_LIVE_OC
-		ret = cpufreq_driver_target(cpufreq_cpu_get(0), sleep_freq,
+	max = policy->max;
+	min = policy->min;
+	policy->max = policy->min = sleep_freq;
+		ret = cpufreq_driver_target(policy, sleep_freq,
 				DISABLE_FURTHER_CPUFREQ);
 #else
-		ret = cpufreq_driver_target(cpufreq_cpu_get(0), SLEEP_FREQ,
+	max = policy->max;
+	min = policy->min;
+	policy->max = policy->min = SLEEP_FREQ;
+		ret = cpufreq_driver_target(policy, SLEEP_FREQ,
 				DISABLE_FURTHER_CPUFREQ);
 #endif
 		if (ret < 0)
@@ -856,11 +865,15 @@ static int s5pv210_cpufreq_notifier_event(struct notifier_block *this,
 #ifdef CONFIG_LIVE_OC
 		pllbus_changing = true;
 
-		cpufreq_driver_target(cpufreq_cpu_get(0), sleep_freq,
+		cpufreq_driver_target(policy, sleep_freq,
 				ENABLE_FURTHER_CPUFREQ);
+	policy->max = max;
+	policy->min = min;
 #else
-		cpufreq_driver_target(cpufreq_cpu_get(0), SLEEP_FREQ,
+		cpufreq_driver_target(policy, SLEEP_FREQ,
 				ENABLE_FURTHER_CPUFREQ);
+	policy->max = max;
+	policy->min = min;
 #endif
 		return NOTIFY_OK;
 	}
